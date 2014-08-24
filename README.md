885df95547fc2dc82bce6b83bd1abfea4634b75f
liaohuqiu <liaohuqiu@gmail.com>
2014-08-24 22:16:20 +0800

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
-binpack-java
-============
+### binpack-java
 
 The Java implement for binpack.
 
-More information about binpack: [http://binpack.liaohuqiu.net/](http://binpack.liaohuqiu.net/)
+SPEC: [http://binpack.liaohuqiu.net/](http://binpack.liaohuqiu.net/)
+
+
+### usage:
+
+```xml
+<dependency>
+    <groupId>in.srain</groupId>
+    <artifactId>binpack</artifactId>
+    <version>1.0.1</version>
+</dependency>
+```
+
+```java
+String strToBs = "All men are created equal.";
+Map<String, Object> m = new HashMap<String, Object>();
+m.put("abc", 32);
+m.put("hello", "You jump, I jump");
+m.put("world", strToBs.getBytes("UTF-8"));
+m.put("float", 12345.6789E123);
+
+byte[] tmp = BinPack.encode(m, "UTF-8");
+Map<String, Object> obj = (Map<String, Object>) BinPack.decode(tmp, "UTF-8");
+byte[] bs = (byte[]) obj.get("world");
+System.out.println(m);
+System.out.println(obj);
+System.out.println(new String(bs, "UTF-8").equals(strToBs));
+```
--- tests/src/Main.java
+++ tests/src/Main.java
@@ -1,37 +1,24 @@
-import in.srain.binpack.BinData;
 import in.srain.binpack.BinPack;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws UnsupportedEncodingException, IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        BinPack.pack(out, out, "UTF-8");
-        BinPack.pack(out, 1234.5f, "UTF-8");
-        System.out.println(BinPack.decode(out.toByteArray(), "UTF-8"));
+    public static void main(String[] args) throws  IOException {
 
+        String strToBs = "All men are created equal.";
         Map<String, Object> m = new HashMap<String, Object>();
         m.put("abc", 32);
         m.put("hello", "You jump, I jump");
-        m.put("world", "All men are created equal".getBytes("UTF-8"));
+        m.put("world", strToBs.getBytes("UTF-8"));
         m.put("float", 12345.6789E123);
 
-        for (int i = 0; i < 1024 * 1024 * 1; ++i) {
-            byte[] tmp = BinPack.encode(m, "GBK");
-        }
-
-        byte[] v = BinPack.encode(m, "GBK");
-        System.out.println(v.length);
-
-        for (int i = 0; i < 1024 * 1024 * 1; ++i) {
-            Object obj = BinPack.decode(v, "GBK");
-        }
-
-        Object obj = BinPack.decode(v, "GBK");
+        byte[] tmp = BinPack.encode(m, "UTF-8");
+        Map<String, Object> obj = (Map<String, Object>) BinPack.decode(tmp, "UTF-8");
+        byte[] bs = (byte[]) obj.get("world");
+        System.out.println(m);
         System.out.println(obj);
+        System.out.println(new String(bs, "UTF-8").equals(strToBs));
     }
 }
